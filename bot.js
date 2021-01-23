47b7c8d52514374b3d48dd6c12690556f4c94f97
TchTech <67865526+TchTech@users.noreply.github.com>
2021-01-24 01:19:40 +0300

    fall system start

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const configfile = require('./data/config.json')
 const prefix = configfile.prefix
 const token = configfile.token
 const commands = require('./commands.json')
+const falls_of_users = {}
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -12,12 +13,32 @@ client.on('ready', () => {
 // Create an event listener for messages
 client.on('message', message => {
   if (message.content.split(" ")[0] === commands.repeat) {
-    //let user = message.mentions.members.first();
-    //console.log(user)
+    let user = message.mentions.members.first();
+    console.log(user.kick())
     let textCommand = message.content.split(" ")
     let deletedElement = textCommand.splice(0, 1)
     message.reply(message.author.username + ": " + textCommand.join(" "));
   }
 });
+
+client.on('message', message => {
+  if (message.content.split(" ")[0] === commands.gfall){
+    //if (message.member.roles.find(role => role.name === 'The Boyare')){}
+    if (typeof(message.content.split(" ")[2]) !== 'string'){
+      message.reply('clarify the user\'s misconduct with a comment {b!fall <username> <comment>}')
+      
+    }else{
+    let user = message.mentions.members.first();
+    falls_of_users[user] = (falls_of_users[user] || 0) + 1
+    if(falls_of_users[user] >= 3){
+      user.kick()
+      message.reply('User has been kicked, because user has collected 3 falls!')
+    }
+    else{
+      message.reply(user.user.username + ' has already collected ' + falls_of_users[user] + ' falls in case of ' + message.content.split(" ").slice(2).join(" ") + "!")
+    }
+  }
+  }
+})
 
 client.login(token);
